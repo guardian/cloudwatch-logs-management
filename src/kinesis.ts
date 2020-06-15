8ce0302e4f585f35d9659d089fa2d41cddb596c8
Mullefa <guy.dawson@guardian.co.uk>
2020-06-15 17:24:12 +0100

    batch requests with size no more than 500

--- src/kinesis.ts
+++ src/kinesis.ts
@@ -1,15 +1,25 @@
 import { Kinesis } from "aws-sdk";
-import { PutRecordsRequestEntry } from "aws-sdk/clients/kinesis";
+import { PutRecordsOutput, PutRecordsRequestEntry } from "aws-sdk/clients/kinesis";
 
-export async function putKinesisRecords(kinesis: Kinesis, streamName: string, logEntries: PublishableStructuredLogData[]): Promise<void> {
+export async function putKinesisRecords(kinesis: Kinesis, streamName: string, logEntries: PublishableStructuredLogData[]): Promise<PutRecordsOutput[]> {
     const records: PutRecordsRequestEntry[] = logEntries.map((entry) => {
         return {
             Data: JSON.stringify(entry),
             PartitionKey: entry.cloudwatchId
         }
     });
-    await kinesis.putRecords({
-        Records: records,
+
+    // Batch records since each put record request supports at most 500 records
+    // https://docs.aws.amazon.com/kinesis/latest/APIReference/API_PutRecords.html
+    const batchRecords: PutRecordsRequestEntry[][] = [];
+    while (records.length > 0) {
+        batchRecords.push(records.splice(0, 500));
+    }
+
+    const results = batchRecords.map(batch => kinesis.putRecords({
+        Records: batch,
         StreamName: streamName,
-    }).promise();
+    }).promise());
+
+    return await Promise.all(results);
 }
