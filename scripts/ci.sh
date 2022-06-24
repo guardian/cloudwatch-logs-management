54c8fa5e49416ddf89d9d7bbe57af052f197b3af
Akash Askoolum <akash1810@gmail.com>
2022-06-24 10:48:34 +0100

    ci: Use @guardian/node-riffraff-artifact to simplify CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
-# This script will build the SAM app and push into the appropriate bucket
-# It will then try to push the resulting cloudformation into a riff-raff build
 
 set -e
 
-echo $BASH_SOURCE
-SCRIPT_DIR=$(dirname ${BASH_SOURCE})
-pushd ${SCRIPT_DIR}/..
-PROJECT_DIR=$(pwd)
-
-function cleanup {
-    popd
-}
-trap cleanup EXIT
-
-# create riff-raff dir
-RIFF_RAFF_ARTIFACT_DIR=${PROJECT_DIR}/riff-raff-artifact
-rm -rf ${RIFF_RAFF_ARTIFACT_DIR} || true
-pwd
-mkdir -p ${RIFF_RAFF_ARTIFACT_DIR}/cloudwatch-logs-management
-
-set +x
 # source NVM on teamcity
 if [ -e "${NVM_DIR}/nvm.sh" ]; then
     . ${NVM_DIR}/nvm.sh
@@ -40,16 +21,4 @@ BUILD="${BUILD_NUMBER:-DEV}"
 echo "export const BUILD_INFO = { 'ShippedBy-revision': '${COMMIT}', 'ShippedBy-buildNumber': '${BUILD}' };"  > src/build-info.ts
 
 npm run build
-set -x
-# bundle lambda code
-(
-    cd ${PROJECT_DIR}/dist
-    zip -r ${RIFF_RAFF_ARTIFACT_DIR}/cloudwatch-logs-management/lambda.zip *
-)
-
-mkdir -p ${RIFF_RAFF_ARTIFACT_DIR}/cloudwatch-logs-management-cfn
-cp ${PROJECT_DIR}/template.yaml ${RIFF_RAFF_ARTIFACT_DIR}/cloudwatch-logs-management-cfn/template.yaml
-cp ${PROJECT_DIR}/riff-raff.yaml ${RIFF_RAFF_ARTIFACT_DIR}/riff-raff.yaml
-
-# publish from teamcity
-echo "##teamcity[publishArtifacts '${RIFF_RAFF_ARTIFACT_DIR} => .']"
+npm run riffRaffUpload
