d7f2de5c37928f39e095733039caae338c821481
Akash Askoolum <akash1810@gmail.com>
2022-06-24 11:11:30 +0100

    refactor: Refactor the CI script for added clarity

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,23 +2,28 @@
 
 set -e
 
-# source NVM on teamcity
-if [ -e "${NVM_DIR}/nvm.sh" ]; then
-    . ${NVM_DIR}/nvm.sh
-else
-    . $(brew --prefix nvm)/nvm.sh
-fi
-nvm install
-nvm use
+setupNodeVersion() {
+  # source NVM on teamcity
+  if [ -e "${NVM_DIR}/nvm.sh" ]; then
+      . ${NVM_DIR}/nvm.sh
+  else
+      . $(brew --prefix nvm)/nvm.sh
+  fi
+  nvm install
+  nvm use
+}
 
-# install deps, run tests and build app
+injectBuildInfo() {
+  COMMIT=$(git rev-parse HEAD)
+  BUILD="${BUILD_NUMBER:-DEV}"
+  echo "export const BUILD_INFO = { 'ShippedBy-revision': '${COMMIT}', 'ShippedBy-buildNumber': '${BUILD}' };"  > src/build-info.ts
+}
+
+setupNodeVersion
+injectBuildInfo
+
 npm ci
 npm run test
 npm run lint
-
-COMMIT=$(git rev-parse HEAD)
-BUILD="${BUILD_NUMBER:-DEV}"
-echo "export const BUILD_INFO = { 'ShippedBy-revision': '${COMMIT}', 'ShippedBy-buildNumber': '${BUILD}' };"  > src/build-info.ts
-
 npm run build
 npm run riffRaffUpload
