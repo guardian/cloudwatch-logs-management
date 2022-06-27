e302c019bf3c24a549c203f08086fb5790f6ae85
Akash Askoolum <akash1810@gmail.com>
2022-06-27 20:23:55 +0100

    feat: Generate the riff-raff.yaml file from the CDK definition
    GuCDK will produce 1 JSON template file per stack, each with a different filename.
    
    This means we cannot use `templates` within `riff-raff.yaml` as the CFN deployment is no longer generic.
    
    This change will generate the `riff-raff.yaml` from the CDK app.

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -1,11 +1,21 @@
 import 'source-map-support/register';
+import type { StageSynthesisOptions } from 'aws-cdk-lib';
 import { App } from 'aws-cdk-lib';
+import type { CloudAssembly } from 'aws-cdk-lib/cx-api';
 import type { CloudwatchLogsManagementProps } from '../lib/cloudwatch-logs-management';
 import { CloudwatchLogsManagement } from '../lib/cloudwatch-logs-management';
+import { RiffRaffYamlFile } from '../lib/riff-raff-yaml';
 
-const app = new App();
+class AppWithRiffRaffYamlGenerator extends App {
+	override synth(options?: StageSynthesisOptions): CloudAssembly {
+		new RiffRaffYamlFile(this).synth();
+		return super.synth(options);
+	}
+}
 
-const stacks: CloudwatchLogsManagementProps[] = [
+const app = new AppWithRiffRaffYamlGenerator();
+
+export const stacks: CloudwatchLogsManagementProps[] = [
 	{
 		stack: 'deploy',
 		cloudFormationStackName: 'deploy-PROD-cloudwatch-logs-management',
--- packages/cdk/lib/cloudwatch-logs-management.test.ts
+++ packages/cdk/lib/cloudwatch-logs-management.test.ts
@@ -1,6 +1,8 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
+import { stacks } from '../bin/cdk';
 import { CloudwatchLogsManagement } from './cloudwatch-logs-management';
+import { RiffRaffYamlFile } from './riff-raff-yaml';
 
 describe('The CloudwatchLogsManagement stack', () => {
 	it('matches the snapshot', () => {
@@ -8,5 +10,15 @@ describe('The CloudwatchLogsManagement stack', () => {
 		const stack = new CloudwatchLogsManagement(app, { stack: 'deploy' });
 		const template = Template.fromStack(stack);
 		expect(template.toJSON()).toMatchSnapshot();
+	});
+});
+
+describe('The riff-raff.yaml file', () => {
+	it('matches the snapshot', () => {
+		const app = new App({ outdir: '/tmp/cdk.out' });
+		stacks.forEach((stack) => new CloudwatchLogsManagement(app, stack));
+
+		const riffRaff = new RiffRaffYamlFile(app);
+		expect(riffRaff.toYAML()).toMatchSnapshot();
 	});
 });
