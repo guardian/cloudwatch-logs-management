f4323502ac8095f9ce81f406d321afa805303c02
Jacob Winch <jacob.winch@guardian.co.uk>
2025-09-02 09:29:20 +0100

    Update packages/cdk/lib/cloudwatch-logs-management.ts
    Co-authored-by: Akash Askoolum <akash1810@users.noreply.github.com>

--- packages/cdk/lib/cloudwatch-logs-management.ts
+++ packages/cdk/lib/cloudwatch-logs-management.ts
@@ -3,8 +3,7 @@ import { GuStack, GuStringParameter } from '@guardian/cdk/lib/constructs/core';
 import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import { GuS3Bucket } from '@guardian/cdk/lib/constructs/s3';
 import type { App } from 'aws-cdk-lib';
-import { Tags } from 'aws-cdk-lib';
-import { Duration } from 'aws-cdk-lib';
+import { Duration, Tags } from 'aws-cdk-lib';
 import { Schedule } from 'aws-cdk-lib/aws-events';
 import {
 	Effect,
